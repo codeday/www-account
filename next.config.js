55c700a58a8ca17f6c01176e1c6704ae5d737dfc
Nikhil Garg <nikhil@gargmail.com>
2023-03-29 20:55:38 -0700

    use recommended nextjs dockerfile - 1/10 the image size

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { withTopo } = require('@codeday/topo/Next');
 
+/** @type {import('next').NextConfig} */
 module.exports = withTopo({
+  output: 'standalone',
   serverRuntimeConfig: {
     auth0: {
       clientId: process.env.AUTH0_CLIENT_ID,
